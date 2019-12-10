4e388f03b28d28e414213b2e1be85c4b8f13dfd5
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2019-12-10 14:28:49 +0000

    Add blurb to top of program.

--- SafetyDump/Program.cs
+++ SafetyDump/Program.cs
@@ -3,6 +3,23 @@ using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Text;
 
+/*
+ * SafetyDump is an in-memory process memory dumper.
+ * 
+ * This uses the Minidump Windows API to dump process memory before base64 encoding that dump and writing it to standard output.
+ * This allows the dump to be redirected to a file or straight back down C2 or through other tools.
+ * 
+ * Note as everything takes place in memory this can be memory intensive while the dumping is taking place.
+ * 
+ * See its integration with https://github.com/nettitude/PoshC2 for how it can be used to dump process memory down C2 without the executable or the dump touching disk.
+ *
+ * Running the compiled binary without any arguments will find and dump lsass.exe.
+ * 		./SafetyDump.exe
+ *
+ * Passing a PID to the program will instead dump that process.
+ * 		./SafetyDump.exe <processIdToDump>
+ * 
+ */
 namespace SafetyDump
 {
     internal static class Program
@@ -91,7 +108,6 @@ namespace SafetyDump
             }
             Console.WriteLine("[-] Dump failed");
             Console.WriteLine(Marshal.GetLastWin32Error());
-
         }
     
         private static void Main(string[] args)
